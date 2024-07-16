6893a32c797263e44554b48b982b0fe0d7917abe
Assaf Dori <assafxdori@gmail.com>
2024-07-16 18:42:15 +0300

    commit

--- infrastructure/instances.tf
+++ infrastructure/instances.tf
@@ -21,3 +21,15 @@ resource "aws_instance" "mongo-express" {
     Name = "${var.env}-mongo-express"
   }
 }
+
+resource "aws_instance" "bastion" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
+  
+  tags = {
+    Name = "${var.env}-bastion"
+  }
+}
--- infrastructure/security-groups.tf
+++ infrastructure/security-groups.tf
@@ -42,3 +42,26 @@ resource "aws_security_group" "mongoexpress-sg" {
     Name = "${var.env}-mongoexpress-sg"
   }
 }
+
+resource "aws_security_group" "bastion-sg" {
+  name = "${var.env}-bastion-sg"
+  vpc_id = aws_vpc.main.id
+  
+    ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-bastion-sg"
+  }
+}
